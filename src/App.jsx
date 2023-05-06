23bb7f7159fadda905e662f8bff7c88fcffb413d
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2023-05-07 01:10:12 +0200

    add deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Layout from './components/page-layout';
 import Home from './pages/home';

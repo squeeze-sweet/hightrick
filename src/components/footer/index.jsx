807a1aea76200c1c0290bd17b15f9649538fd1b7
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2023-05-07 09:49:31 +0200

    fix footer icons and button cursor

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -12,22 +12,10 @@ export default function Footer() {
         <p className={styles.logoText}>Hightrick</p>
       </div>
       <div className={styles.right}>
-        <IconWithText
-          iconScr={process.env.PUBLIC_URL + '/whatsapp.svg'}
-          text="+7(912) 272-04-09"
-        />
-        <IconWithText
-          iconScr={process.env.PUBLIC_URL + '/vk.svg'}
-          text="Vkontakte"
-        />
-        <IconWithText
-          iconScr={process.env.PUBLIC_URL + '/youtube.svg'}
-          text="YouTube"
-        />
-        <IconWithText
-          iconScr={process.env.PUBLIC_URL + '/gmail.svg'}
-          text="Gmail"
-        />
+        <IconWithText iconScr={'/whatsapp.svg'} text="+7(912) 272-04-09" />
+        <IconWithText iconScr={'/vk.svg'} text="Vkontakte" />
+        <IconWithText iconScr={'/youtube.svg'} text="YouTube" />
+        <IconWithText iconScr={'/gmail.svg'} text="Gmail" />
       </div>
     </section>
   );

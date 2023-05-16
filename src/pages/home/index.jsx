cedcf680da239ca029cc2810adbe3bf58f52e785
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2023-05-16 23:14:55 +0200

    Перенёс видео на главную

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,36 @@
 import Button from '../../components/button';
+import ReactPlayer from 'react-player';
+import { useEffect, useRef, useState } from 'react';
 import styles from './index.module.css';
 
 export default function Home() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const videoRef = useRef(null);
+  useEffect(() => {
+    if (videoRef.current && !isPlaying) {
+      const handleScroll = () => {
+        const viewportHeight = window.innerHeight;
+        const imageElement = videoRef.current;
+        if (imageElement) {
+          const { top, bottom, height } = imageElement.getBoundingClientRect();
+          const imagePosition = top + height / 2;
+          const isCentered =
+            imagePosition >= viewportHeight / 2 &&
+            imagePosition <= viewportHeight / 2 + 1; // Устанавливаем погрешность в 1 пиксель
+          setIsPlaying(true);
+        }
+      };
+      window.addEventListener('scroll', handleScroll);
+      window.addEventListener('resize', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener('resize', handleScroll);
+      };
+    }
+  }, [videoRef.current, isPlaying]);
+
   return (
     <>
       <section className={styles.mainImgContainer}>
@@ -29,12 +58,21 @@ export default function Home() {
         заключается в выполнении различных трюков с футбольным мячом. Для этого
         не нужно иметь талант.
       </p>
-      <div className={styles.balls}>
-        <img
-          className={styles.img}
-          src={process.env.PUBLIC_URL + '/balls.png'}
-          alt="чисто мячи"
-        />
+
+      <div ref={videoRef} className={styles.iframeContainer}>
+        <div className={styles.iframeFiller}> </div>
+        <ReactPlayer
+          url="https://www.youtube.com/embed/w6iHoQXTSYA"
+          playing={isPlaying}
+          muted={!isPlaying}
+          width="100%"
+          height="100%"
+          style={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+          }}
+        ></ReactPlayer>
       </div>
       <div className={styles.buttonContainer}>
         <Button text="ЗАПИСАТЬСЯ" />

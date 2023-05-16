e4185ff701f454a7424d3b863302fc38ee1dc3d3
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2023-05-16 23:32:34 +0200

    Поправил вид карточек

--- src/components/photo-card/index.jsx
+++ src/components/photo-card/index.jsx
@@ -3,15 +3,17 @@ import styles from './photo-card.module.css';
 export default function PhotoCard({ photoSrc, heading, text, className }) {
   return (
     <div className={`${styles.card} + ${className}`}>
-      <img
-        className={styles.img}
-        src={process.env.PUBLIC_URL + photoSrc}
-        alt=""
-      />
+      <div className={styles.imgContainer}>
+        <img
+          className={styles.img}
+          src={process.env.PUBLIC_URL + photoSrc}
+          alt=""
+        />
+      </div>
 
       <div className={styles.textContainer}>
         <h3 className={styles.heading}>{heading}</h3>
-        <p>{text}</p>
+        <p className={styles.text}>{text}</p>
       </div>
     </div>
   );

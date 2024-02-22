6629adbaa0a80f2bbe009a11fb38331105ea54fe
Alexander Ilinykh <AlexanderIlinykhDev@yandex.ru>
2024-02-22 16:20:14 +0100

    adapt carousel, change photoes

--- src/pages/goods/index.jsx
+++ src/pages/goods/index.jsx
@@ -2,9 +2,10 @@ import styles from './index.module.css';
 import PhotoCard from '../../components/photo-card';
 import Button from '../../components/button';
 import Form from '../../components/form';
-import { useCallback, useState } from 'react';
+import { useCallback, useState, useMemo } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { useWindowSize } from '@uidotdev/usehooks';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -16,27 +17,43 @@ export default function Goods() {
   const showModal = useCallback(() => {
     setIsModalOpen(true);
   }, [setIsModalOpen]);
+
+  const { width } = useWindowSize();
+
+  const visibleCardsNumber = useMemo(() => {
+    if (width < 300) return 1;
+    if (width < 600) return 2;
+    return 3;
+  }, [width]);
+
+  const bulletSize = useMemo(() => {
+    if (width < 300) return '6px';
+    if (width < 600) return '8px';
+    return '16px';
+  }, [width]);
+
+  const arrowSize = useMemo(() => {
+    if (width < 300) return '34px';
+    if (width < 600) return '24px';
+    return '44px';
+  }, [width]);
+
+  console.log(width);
   return (
     <div className={styles.page}>
       <Form isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
       <h2 className={styles.header}>наш Мерч</h2>
-      <p className={styles.text}>
-        Выражайте уникальность и завоевывайте внимание! Наши фирменные футболки
-        – это не просто одежда, это выражение свободы и творчества,
-        олицетворение стиля и искусства!
-      </p>
-      <p className={styles.text}>Состав: 100% хлопок</p>
-
       <div className={styles.swiperContainer}>
         <Swiper
           style={{
             '--swiper-pagination-color': '#d41fa2',
             '--swiper-pagination-bullet-inactive-color': '#999999',
             '--swiper-pagination-bullet-inactive-opacity': '1',
-            '--swiper-pagination-bullet-size': '16px',
+            '--swiper-pagination-bullet-size': bulletSize,
             '--swiper-pagination-bullet-horizontal-gap': '6px',
+            '--swiper-navigation-size': arrowSize,
           }}
-          slidesPerView={3}
+          slidesPerView={visibleCardsNumber}
           spaceBetween={30}
           loop={true}
           pagination={{
@@ -50,10 +67,10 @@ export default function Goods() {
             <img src={process.env.PUBLIC_URL + '/c1(1).png'} alt="image" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={process.env.PUBLIC_URL + '/c1(2).png'} alt="image" />
+            <img src={process.env.PUBLIC_URL + '/c1(3).png'} alt="image" />
           </SwiperSlide>
           <SwiperSlide>
-            <img src={process.env.PUBLIC_URL + '/c1(3).png'} alt="image" />
+            <img src={process.env.PUBLIC_URL + '/c1(2).png'} alt="image" />
           </SwiperSlide>
           <SwiperSlide>
             <img src={process.env.PUBLIC_URL + '/c1(4).png'} alt="image" />
@@ -61,12 +78,14 @@ export default function Goods() {
           <SwiperSlide>
             <img src={process.env.PUBLIC_URL + '/c1(5).png'} alt="image" />
           </SwiperSlide>
-          <SwiperSlide>
-            <img src={process.env.PUBLIC_URL + '/c1(6).png'} alt="image" />
-          </SwiperSlide>
         </Swiper>
       </div>
-
+      <p className={styles.text}>
+        Выражайте уникальность и завоевывайте внимание! Наши фирменные футболки
+        – это не просто одежда, это выражение свободы и творчества,
+        олицетворение стиля и искусства!
+      </p>
+      <p className={styles.text}>Состав: 100% хлопок</p>
       <p className={styles.contact}>
         По любым интересующим Вас вопросам Вы можете обращаться по номеру:
         8(912)-272-04-09 - Ришат

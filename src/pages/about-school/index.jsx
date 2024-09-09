35f3ba1fb9b2811eff4ae5e51c39addd646196d1
AnthonyAspen <antonosintsev2000@gmail.com>
2024-09-09 14:02:44 +0500

    update prices

--- src/pages/about-school/index.jsx
+++ src/pages/about-school/index.jsx
@@ -1,9 +1,9 @@
-import styles from './index.module.css';
-import InfoCard from '../../components/info-card';
-import Map from '../../components/map';
-import Button from '../../components/button';
-import Form from '../../components/form';
-import { useCallback, useState } from 'react';
+import styles from "./index.module.css";
+import InfoCard from "../../components/info-card";
+import Map from "../../components/map";
+import Button from "../../components/button";
+import Form from "../../components/form";
+import { useCallback, useState } from "react";
 
 export default function AboutSchool() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -23,12 +23,12 @@ export default function AboutSchool() {
         <div className={styles.left}>
           <h3>Стоимость занятий</h3>
           <InfoCard heading="Первая тренировка" text="Бесплатно" highlighted />
-          <InfoCard heading="Разовое посещение" text="600 рублей" />
+          <InfoCard heading="Разовое посещение" text="750 рублей" />
           <InfoCard
             heading="Абонемент на месяц"
             text={[
-              '• 3500 рублей (2 раза в неделю)',
-              '• 5250 рублей (3 раза в неделю)',
+              "• 4000 рублей (2 раза в неделю)",
+              "• 6000 рублей (3 раза в неделю)",
             ]}
           />
         </div>
@@ -36,7 +36,7 @@ export default function AboutSchool() {
           <h3>Расписание</h3>
           <InfoCard
             heading="Суббота:"
-            text={['• c 18:30 до 19:30 ', '• c 19:30 до 20:30 ']}
+            text={["• c 18:30 до 19:30 ", "• c 19:30 до 20:30 "]}
           />
           <InfoCard heading="Воскресенье:" text="• c 19:45 до 20:45" />
         </div>
